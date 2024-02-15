7ff3176a2df48e968ba0727ed611a5316ad7db11
Joell Malone <joellmalone000@gmail.com>
2024-02-14 20:42:12 -0500

    finally completed the lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+ function distanceFromHqInBlocks(block) {
+  const headquarters = 42
+  return Math.abs(block - headquarters)
+};
+function distanceFromHqInFeet(distance) {
+    const block = distanceFromHqInBlocks(distance)
+    const feet = 264
+    return Math.abs(block * feet)
+};
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination)
+   if(distance < 400) {
+    return 0
+   }else if(distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02
+   }else if(distance <= 2500) {
+    return 25
+   } else {
+    return 'cannot travel that far'
+   }
+}
